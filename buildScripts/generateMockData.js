b3b261fb6c252594a6c1f8f94d928a892f782282
Chris Biles <chris@gravlens.com>
2022-02-14 19:41:22 -0500

    Removed commented out code in generateMockApi.js

--- buildScripts/generateMockData.js
+++ buildScripts/generateMockData.js
@@ -5,24 +5,6 @@ Provides randomized data.
 
 /* eslint-disable no-console*/
 
-// eslint-disable-next-line import/named
-// import { generate, extend } from "json-schema-faker";
-// import { schema } from "./mockDataSchema";
-// import fs from "fs";
-// import chalk from "chalk";
-
-// // Extend JSF with the fake libs one wants to use.
-// extend("faker", () => require("faker"));
-// const json = JSON.stringify(generate(schema));
-
-// fs.writeFile("./src/api/db.json", json, function (err) {
-// 	if (err) {
-// 		return console.log(chalk.red(err));
-// 	} else {
-// 		console.log(chalk.green("Mock data generated"));
-// 	}
-// });
-
 const { resolve, extend } = require('json-schema-faker');
 
 // import { generate, extend } from "json-schema-faker";
@@ -42,13 +24,3 @@ resolve(schema).then(sample => {
 		}
 	});
 });
-
-// const json = JSON.stringify(resolve(schema));
-
-// fs.writeFile("./src/api/db.json", json, function (err) {
-// 	if (err) {
-// 		return console.log(chalk.red(err));
-// 	} else {
-// 		console.log(chalk.green("Mock data generated"));
-// 	}
-// });
