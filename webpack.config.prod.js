0759b42d1f3785af4520582a27f801634443288f
Chris Biles <chris@gravlens.com>
2022-02-15 14:44:25 -0500

    Added cache busting and generation of separate css file for prod.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -5,6 +5,7 @@
 
 import path from "path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export default {
 	mode: "production",
@@ -16,18 +17,22 @@ export default {
 	output: {
 		path: path.resolve(__dirname, "dist"),
 		publicPath: "/",
-		filename: "[name].js"
+		filename: "[name].[chunkhash].js"
 	},
 	plugins: [
 		// Create HTML file that includes reference to bundled JS.
 		new HtmlWebpackPlugin({
 			template: "src/index.html"
+		}),
+		// Generate an external css file with a hash in the filename.
+		new MiniCssExtractPlugin({
+			filename: "[name].[chunkhash].css"
 		})
 	],
 	module: {
 		rules: [
 			{test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"]},
-			{test: /\.css$/, use: ["style-loader", "css-loader"]}
+			{test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"]}
 		]
 	}
 }
